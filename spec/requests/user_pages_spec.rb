f196dba5fdda5c9725b304309e04f2b2eb34055a
Chris Jacob <i.chris.jacob@gmail.com>
2013-08-10 00:52:30 +1000

    Finish user index and destroy actions. Gemfile :production using rails_12factor gem

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,7 +5,9 @@ describe "User pages" do
 	subject { page }
 
 	describe "index" do
+		
 		let(:user) { FactoryGirl.create(:user) }
+		
 		before(:each) do
 			sign_in user
 			visit users_path
@@ -24,6 +26,40 @@ describe "User pages" do
 			it "should list each user" do
 				User.paginate(page: 1).each do |user|
 					expect(page).to have_selector('li', text: user.name)
+				end
+			end
+		end
+
+		describe "delete links" do
+
+			it { should_not have_link('delete') }
+
+			describe "as an admin user" do
+				let(:admin) { FactoryGirl.create(:admin) }
+				before do
+					sign_in admin
+					visit users_path
+				end
+
+				it { should have_link('delete', href: user_path(User.first)) }
+				it "should be able to delete another user" do
+					expect do
+						click_link('delete', match: :first)
+					end.to change(User, :count).by(-1)
+				end
+
+				it { should_not have_link('delete', href: user_path(admin)) }
+			end
+
+			describe "as non-admin user" do
+				let(:user) { FactoryGirl.create(:user) }
+				let(:non_admin) { FactoryGirl.create(:user) }
+
+				before { sign_in non_admin, no_capybara: true }
+
+				describe "submitting a DELETE request to the Users#destroy action" do
+					before { delete user_path(user) }
+					specify { expect(response).to redirect_to(root_url) }
 				end
 			end
 		end
